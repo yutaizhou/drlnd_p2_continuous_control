36a16c2c94f5fa08db57a63d13f8f23ec5cc3e92
Yutai Zhou <yutai.zhou@ll.mit.edu>
2021-01-29 08:29:59 -0500

    update frequency 10 times every 20 steps, added method to process buffer samples into batches

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -13,7 +13,7 @@ from ..utils.utils import DEVICE
 
 
 BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 512        # minibatch size
+BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 LR_ACTOR = 1e-4         # learning rate of the actor 
 LR_CRITIC = 3e-4        # learning rate of the critic
@@ -22,7 +22,7 @@ WEIGHT_DECAY = 0.0001   # L2 weight decay
 
 
 class DDPG():
-    def __init__(self, state_size, action_size, random_seed=42):
+    def __init__(self, state_size, action_size, num_agents=1, random_seed=42):
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(random_seed)
@@ -40,12 +40,15 @@ class DDPG():
         # Misc
         self.noise = OUNoise(action_size, random_seed)
         self.memory = ReplayBuffer(BUFFER_SIZE, BATCH_SIZE, random_seed)
+        self.num_batches = int(num_agents/2)
+        self.t = 0
     
     def step(self, state, action, reward, next_state, done):
         self.memory.add(state, action, reward, next_state, done)
-        if len(self.memory) > BATCH_SIZE:
-            experiences = self.memory.sample()
+        if (self.t % 20 == 0) & (len(self.memory) > BATCH_SIZE * self.num_batches):
+            experiences = self.memory.sample(self.num_batches)
             self._learn(experiences, GAMMA)
+        self.t += 1
 
     def act(self, state, add_noise=True):
         state = torch.from_numpy(state).float().to(DEVICE)
@@ -62,35 +65,46 @@ class DDPG():
     def reset(self):
         self.noise.reset()
     
-    def _learn(self, experiences, gamma):
-        states, actions, rewards, next_states, dones = experiences
-
-        # update critic
-        actions_next = self.actor_target(next_states)
-        Q_targets_next = self.critic_target(next_states, actions_next)
-        Q_targets = rewards + gamma * Q_targets_next * (1-dones)
-
-        Q_expected = self.critic_local(states, actions)
-
-        critic_loss = F.mse_loss(Q_expected, Q_targets)
-        self.critic_opt.zero_grad()
-        critic_loss.backward()
-        self.critic_opt.step()
-
-        # update actor
-        actions_pred = self.actor_local(states)
-
-        actor_loss = -self.critic_local(states, actions_pred).mean()
-        self.actor_opt.zero_grad()
-        actor_loss.backward()
-        self.actor_opt.step()
-
-
-        # target network upates
-        self._soft_update(self.actor_local, self.actor_target, TAU)
-        self._soft_update(self.critic_local, self.critic_target, TAU)
+    @staticmethod
+    def _batch_experiences(experiences):
+        all_states, all_actions, all_rewards, all_next_states, all_dones = experiences
+        b_states = torch.split(all_states, BATCH_SIZE)
+        b_actions = torch.split(all_actions, BATCH_SIZE)
+        b_rewards = torch.split(all_rewards, BATCH_SIZE)
+        b_next_states = torch.split(all_next_states, BATCH_SIZE)
+        b_dones = torch.split(all_dones, BATCH_SIZE)
+        return b_states, b_actions, b_rewards, b_next_states, b_dones 
     
-    def _soft_update(self, local_model, target_model, tau):
+    def _learn(self, experiences, gamma):
+        b_states, b_actions, b_rewards, b_next_states, b_dones = self._batch_experiences(experiences)
+
+        for states, actions, rewards, next_states, dones in zip(b_states, b_actions, b_rewards, b_next_states, b_dones):
+            # update critic
+            actions_next = self.actor_target(next_states)
+            Q_targets_next = self.critic_target(next_states, actions_next)
+            Q_targets = rewards + gamma * Q_targets_next * (1-dones)
+
+            Q_expected = self.critic_local(states, actions)
+
+            critic_loss = F.mse_loss(Q_expected, Q_targets)
+            self.critic_opt.zero_grad()
+            critic_loss.backward()
+            self.critic_opt.step()
+
+            # update actor
+            actions_pred = self.actor_local(states)
+
+            actor_loss = -self.critic_local(states, actions_pred).mean()
+            self.actor_opt.zero_grad()
+            actor_loss.backward()
+            self.actor_opt.step()
+
+            # target network upates
+            self._soft_update(self.actor_local, self.actor_target, TAU)
+            self._soft_update(self.critic_local, self.critic_target, TAU)
+    
+    @staticmethod
+    def _soft_update(local_model, target_model, tau):
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             mixed_param = tau * local_param.data + (1-tau)*target_param.data
             target_param.data.copy_(mixed_param)
