d07b2d7f866379539d7c0b582d1af5429cd709ca
Yutai Zhou <yutai.zhou@ll.mit.edu>
2021-01-28 21:22:39 -0500

    switch up default ddpg model

--- src/agents/model.py
+++ src/agents/model.py
@@ -4,42 +4,43 @@ import torch.nn.functional as F
 
 
 class Actor(nn.Module):
-    def __init__(self, state_size, action_size, hidden_size=256, seed=0):
+    def __init__(self, state_size, action_size, hidden_sizes=[256,256], seed=0):
         super().__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, action_size)
+        self.fc1 = nn.Linear(state_size, hidden_sizes[0])
+        self.fc2 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
+        self.fc3 = nn.Linear(hidden_sizes[1], action_size)
         self.init_params()
 
     def init_params(self):
         nn.init.xavier_uniform_(self.fc1.weight.data)
-        nn.init.uniform_(self.fc2.weight.data, -3e-3, 3e-3)
+        nn.init.xavier_uniform_(self.fc2.weight.data)
+        nn.init.uniform_(self.fc3.weight.data, -3e-3, 3e-3)
 
     def forward(self, state):
         x = F.relu(self.fc1(state))
-        x = torch.tanh(self.fc2(x))
+        x = F.relu(self.fc2(x))
+        x = torch.tanh(self.fc3(x))
         return x
 
 class Critic(nn.Module):
-    def __init__(self, state_size, action_size, hidden_sizes = [256, 256, 128], seed=0):
+    def __init__(self, state_size, action_size, hidden_sizes = [256, 256], seed=0):
         super().__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, hidden_sizes[0])
         self.fc2 = nn.Linear(hidden_sizes[0] + action_size, hidden_sizes[1])
-        self.fc3 = nn.Linear(hidden_sizes[1], hidden_sizes[2])
-        self.fc4 = nn.Linear(hidden_sizes[2], 1)
+        self.fc3 = nn.Linear(hidden_sizes[1], 1)
         self.init_params()
     
     def init_params(self):
         nn.init.xavier_uniform_(self.fc1.weight.data) 
         nn.init.xavier_uniform_(self.fc2.weight.data) 
-        nn.init.xavier_uniform_(self.fc3.weight.data) 
-        nn.init.uniform_(self.fc4.weight.data, -3e-3, 3e-3)
+        nn.init.uniform_(self.fc3.weight.data, -3e-3, 3e-3)
     
     def forward(self, state, action):
         """Q-Value critic"""
-        state_input = F.leaky_relu(self.fc1(state))
+        state_input = F.relu(self.fc1(state))
         x = torch.cat((state_input, action), dim=-1)
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
-        return self.fc4(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return 
