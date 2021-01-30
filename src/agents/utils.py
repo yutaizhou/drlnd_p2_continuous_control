fff8613e65ade6bbb8b9aa1fd0ad6128fe7505ab
Yutai Zhou <yutai.zhou@ll.mit.edu>
2021-01-30 08:16:51 -0500

    correcting behavior for OU noise

--- src/agents/utils.py
+++ src/agents/utils.py
@@ -20,7 +20,6 @@ class OUNoise:
     def sample(self):
         """Update internal state and return it as a noise sample."""
         x = self.state
-        # dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
-        dx = self.theta * (self.mu - x) + self.sigma * np.random.random(self.mu.shape)
+        dx = self.theta * (self.mu - x) + self.sigma * (np.random.random(self.mu.shape) - 0.5)
         self.state = x + dx
         return self.state
